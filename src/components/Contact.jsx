68d6109aae71d1eb85b48b0c5981315cb75d2167
farhanazulaikha <farhanazulaikha499@gmail.com>
2024-02-15 22:34:51 +0800

    make contact form able to send email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,44 +1,41 @@
 import Title from "./Title";
 import '../assets/styles/contact.css';
-import { useState } from "react";
+import { useRef, useState } from "react";
+import emailjs from 'emailjs-com';
 
 export default function Contact() {
 
-    const [formDetail, setFormDetail] = useState({
-        fullName: "",
-        email: "",
-        message: ""
-    })
+    const form = useRef();
 
-    const handleChange = (e) => {
-        const { id, value } = e.target;
+    const [fullName, setFullName] = useState("");
+    const [message, setMessage] = useState("");
 
-        setFormDetail((prevState) => ({
-            ...prevState,
-            [id]: value
-        }))
+    const handleName = (e) => {
+        setFullName(e.target.value);
     }
+
+    const handleMessage = (e) => {
+        setMessage(e.target.value);
+    }
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        emailjs.sendForm('service_ccervjr', 'template_wgaxhge', form.current, 'uNpp6Oe7kZ4Brkcxb')
     }
 
     return (
         <div className="contact">
             <Title titleStyle={'contact-title'}>Contact</Title>
             <div className="contact-container">
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit} ref={form}>
                     <div className="form-field">
                         <label htmlFor="name" className="label-field">Name</label>
-                        <input type="text" id="fullName" value={formDetail["fullName"]} onChange={handleChange} required placeholder="Enter Your Name" className="input-field"></input>
-                    </div>
-                    <div className="form-field">
-                        <label htmlFor="email" className="label-field">Email</label>
-                        <input type="email" id="email" value={formDetail["email"]} onChange={handleChange} required placeholder="Enter Your Email" className="input-field"></input>
+                        <input type="text" name="fullName" value={fullName} onChange={handleName} required placeholder="Enter Your Name" className="input-field"></input>
                     </div>
                     <div className="form-field">
                         <label htmlFor="message" className="label-field">Message</label>
-                        <textarea value={formDetail["message"]} id="message" onChange={handleChange} placeholder="Enter Your Message" required className="input-field"></textarea>
+                        <textarea value={message} name="message" onChange={handleMessage} placeholder="Enter Your Message. Please include your contact details." required className="input-field"></textarea>
                     </div>
                     <div className="submit-button">
                         <button id="openModal" type="submit">Submit</button>
