306749bd114a4547803664bad9aeb27a39bbcb28
farhanazulaikha <farhanazulaikha499@gmail.com>
2024-02-17 16:20:57 +0800

    add modal when success when submit message

---
+++ src/components/Modal.jsx
@@ -0,0 +1,24 @@
+import Button from './Button';
+import './modal.css';
+
+export default function Modal({ toggleModal, modal, children }) {  
+    return (
+      <>
+        {modal && (
+          <div className="modal">
+            <div onClick={toggleModal} className="overlay"></div>
+            <div className="modal-content">
+                <p>
+                    { children }
+                </p>
+                <div className="close-modal-div">
+                    <Button buttonStyle={'close-modal'} onClick={toggleModal}>
+                        Close
+                    </Button>
+                </div>
+            </div>
+          </div>
+        )}
+      </>
+    );
+  }
